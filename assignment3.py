1116566d32eda3721c345fa35a4eaed1a0fa9385
LydiaQ1 <112520633+LydiaQ1@users.noreply.github.com>
2022-11-03 18:05:45 -0400

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,11 @@
+import urllib.request
+import json
+from pprint import pprint
+
+MAPQUEST_API_KEY = 'kL5zMDyP7plpwJ8NyY45E9E832gjA6XV'
+
+url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text)
+pprint(response_data)
