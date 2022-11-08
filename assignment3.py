08f1a191cb3ca572ef1974ee77bef665d990b530
YuchengZhang104 <yzhang13@babson.edu>
2022-11-07 21:17:31 -0500

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -26,4 +26,23 @@ def geturl(place=None):
 
 # print(geturl('Wellesley College'))
 
+def getstop(lat,lng):
+    YOUR_MBTA_API_KEY = 'e4b760e47fa14ad0b8f8cbd927c92ec9'
+    url = f'https://api-v3.mbta.com/stops?api_key={YOUR_MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={lat}&filter%5Blongitude%5D={lng}'
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    d = dict()
+    d = response_data
+    stop = d['data'][0]['attributes']['name']
+    wheelchair = d['data'][0]['attributes']['wheelchair_boarding']
+    if wheelchair == 0:
+        wheelchairok = 'is no information of'
+    if wheelchair == 1:
+        wheelchairok = "is"
+    else:
+        wheelchairok = "is no"
+    return f'The nearest MBTA stop is {stop}. There {wheelchairok} wheelchair accessible for this stop'
+
+print(getstop(42.36553,-71.0612))
 
